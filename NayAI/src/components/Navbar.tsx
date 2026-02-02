743d030445b921172a193fa100286c1e5c0d3563
Araya05s <akhsanaraya5@gmail.com>
2026-02-02 23:12:26 +0700

    Fix Navbar responsiveness in mobile and tablet

--- NayAI/src/components/Navbar.tsx
+++ NayAI/src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import { NAV_LINKS } from '../config/navigation'
+import { useState } from "react";
+import MenuIcon from "/icons/menu.svg";
+import CloseIcon from "/icons/close.svg";
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div className='fixed top-0 w-full z-50 bg-neutral-950/80 backdrop-blur-2xl border-2 border-neutral-800'>
-        <div className='max-w-10xl mx-auto px-12 h-18 flex items-center justify-between'>
+        <div className='max-w-10xl mx-auto px-6 lg:px-12 h-18 flex items-center justify-between'>
             <a href="" className='flex items-center'>
                 <img src="/icons/NayAI.svg" alt="" className='h-12 w-12'/>
                 <span className='font-semibold text-3xl tracking-wide'>
@@ -11,7 +17,7 @@ const Navbar = () => {
                 </span>
             </a>
 
-            <div className="flex gap-6 text-sm">
+            <div className="hidden md:flex gap-6 text-sm">
             {NAV_LINKS.map(link => (
                 <a
                 key={link.name}
@@ -22,7 +28,33 @@ const Navbar = () => {
                 </a>
             ))}
             </div>
+
+            <button
+                onClick={() => setOpen(!open)}
+                className="md:hidden relative z-50 w-10 h-10 flex items-center justify-center text-amber-50"
+                >
+                <img
+                    src={open ? CloseIcon : MenuIcon}
+                    alt=""
+                    className={`w-6 h-6 invert transition-transform duration-300 ${open ? "rotate-90" : "rotate-0"}`}
+                />
+            </button>
         </div>
+        <div
+        className={`md:hidden overflow-hidden bg-black/90 transition-all duration-300 ease-out ${open ? "max-h-96 opacity-100" : "max-h-0 opacity-0"} `}>
+            <nav className="flex flex-col px-6 py-6 gap-6">
+            {NAV_LINKS.map(link => (
+                <a
+                key={link.name}
+                href={link.href}
+                onClick={() => setOpen(false)}
+                className="text-neutral-300 hover:text-white transition text-base"
+                >
+                {link.name}
+                </a>
+            ))}
+            </nav>
+      </div>
     </div>
   )
 }
--- NayAI/src/sections/Hero.tsx
+++ NayAI/src/sections/Hero.tsx
@@ -5,12 +5,12 @@ const Hero = () => {
   return (
     <div id="hero" className="min-h-screen flex items-center justify-center text-center px-6">
       <div className="absolute inset-0 z-0 w-full h-full">
-        <img src={Img_Hero} alt="" className="w-full h-full"/>
+        <img src={Img_Hero} alt="" className="w-full h-full object-cover object-left"/>
       </div>
       <div className="absolute inset-0 z-0 w-full h-full">
         <div className="bg-[radial-gradient(ellipse_at_center,transparent_20%,rgba(0,0,0,1)_100%)] w-full h-full"></div>
       </div>
-      <div className="max-w-2xl z-10">
+      <div className="lg:max-w-2xl max-w-sm z-10">
         <h1 className="text-4xl text-neutral-100 md:text-5xl font-bold mb-6">
           {import.meta.env.VITE_APP_NAME}
         </h1>
