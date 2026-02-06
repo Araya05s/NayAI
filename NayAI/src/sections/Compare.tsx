11a6f16284a4dacc60830a4d8dc0181070e1acb4
Araya05s <akhsanaraya5@gmail.com>
2026-02-06 08:44:19 +0700

    Fix compare section image and text position for better reading clarity

--- NayAI/src/sections/Compare.tsx
+++ NayAI/src/sections/Compare.tsx
@@ -5,23 +5,9 @@ import NayAIreveal from "../assets/nayai_result.png"
 const Compare = () => {
   return (
     <section
-        id="about" className="bg-neutral-100 text-neutral-900 py-24"
+        id="about" className="bg-neutral-200 text-neutral-900 py-24"
     >
         <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-2 gap-16 items-center">
-
-            <div>
-                <h2 className="text-4xl font-bold mb-6">
-                Why this tool?
-                </h2>
-
-                <p className="text-neutral-700 leading-relaxed mb-4">
-                Most image processing tools jump straight to the result. You see a “before” and an “after,” but the transformation itself remains invisible from behind — a black box of original image, graphs, and "yes, this is AI".
-                </p>
-
-                <p className="text-neutral-700 leading-relaxed">
-                But NayAI shows the result <b>differently.</b>
-                </p>
-            </div>
             <div className="flex flex-col lg:flex-row gap-12 items-start">
                 <div className="flex-1 space-y-12">
                     <div className="scale-100 hover:scale-110 transition duration-300">
@@ -64,6 +50,19 @@ const Compare = () => {
                             </div>
                         </div>
                 </div>
+            </div>
+            <div>
+                <h2 className="text-4xl font-bold mb-6">
+                Why this tool?
+                </h2>
+
+                <p className="text-neutral-700 leading-relaxed mb-4">
+                Most image processing tools jump straight to the result. You see the original image and the classic "Yes, this is AI image" text, but the reasoning of "why?" itself remains invisible from behind.
+                </p>
+
+                <p className="text-neutral-700 leading-relaxed">
+                But NayAI shows the result <b>differently.</b>
+                </p>
             </div>
         </div>
     </section>
